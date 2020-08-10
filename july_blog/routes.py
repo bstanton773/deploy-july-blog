1d43e33487b32b82406be9fdb7d78fc1ac14d58a
Brian Stanton <brianstanton@Brians-MacBook-Pro.local>
2020-08-10 18:15:05 -0500

    Adding onto morning session, adding base.html, nav.html, register.html, css and updating routes

--- july_blog/routes.py
+++ july_blog/routes.py
@@ -4,4 +4,12 @@ from flask import render_template
 # Home route
 @app.route('/')
 def home():
-    return render_template("home.html")
+    customer_name = "Brian"
+    order_number = 1
+    item_dict = {1:"Ice Cream", 2:"Bread", 3:"Lemons", 4:"Cereal"}
+    return render_template("home.html", customer_name=customer_name, order_number=order_number, item_dict = item_dict)
+
+# Register route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
