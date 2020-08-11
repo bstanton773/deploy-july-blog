5975dfd2a42492689257e97872ec45c327e10a9b
Brian Stanton <brianstanton@Brians-MacBook-Pro.local>
2020-08-11 17:20:53 -0500

    Adding new form and database models

--- july_blog/routes.py
+++ july_blog/routes.py
@@ -1,5 +1,8 @@
 from  july_blog import app
-from flask import render_template
+from flask import render_template, request
+
+# Import for Forms
+from july_blog.forms import UserInfoForm, BlogPostForm
 
 # Home route
 @app.route('/')
@@ -10,6 +13,23 @@ def home():
     return render_template("home.html", customer_name=customer_name, order_number=order_number, item_dict = item_dict)
 
 # Register route
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template("register.html")
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        # Get Information from POST request
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        print("\n",username, password, email)
+    return render_template("register.html", form=form)
+
+# Create Post route
+@app.route('/createposts', methods=['GET', 'POST'])
+def createposts():
+    form = BlogPostForm()
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        content = form.content.data
+        print("\n", title, content)
+    return render_template('createposts.html', form=form)
